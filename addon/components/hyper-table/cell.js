eac89ecfbea11dce6b1e981f1c9107af497c7c8d
Philippe Ndiaye <phndiaye@gmail.com>
2019-09-19 16:05:13 +0200

    header cells shouldn't trigger row click

--- addon/components/hyper-table/cell.js
+++ addon/components/hyper-table/cell.js
@@ -105,7 +105,7 @@ export default Component.extend({
   },
 
   click(e) {
-    if (this.onRowClicked) {
+    if (!this.header && this.onRowClicked) {
       this.onRowClicked(this.item);
     }
   },
