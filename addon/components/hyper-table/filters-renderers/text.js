3aae7a91727c990c6f7eb160df2b344550066782
Mohamed Hegazy <m.hegazy94@hotmail.com>
2019-09-23 17:54:22 +0200

    Add string search filtering to table data

--- addon/components/hyper-table/filters-renderers/text.js
+++ addon/components/hyper-table/filters-renderers/text.js
@@ -1,12 +1,31 @@
 import Component from '@ember/component';
+import { observer,computed } from '@ember/object';
+import { run } from '@ember/runloop';
 
 export default Component.extend({
   classNames: ['available-filters'],
+
+  _searchQuery: computed('column', 'column.filters', function() {
+    let searchTerm = this.column.filters.find((x)=> x.value.term);
+
+    return searchTerm.value.term;
+  }),
 
   orderingOptions: {
     'A — Z': 'alphanumerical:asc',
     'Z — A': 'alphanumerical:desc'
   },
+
+  _searchQueryObserver: observer('_searchQuery', function() {
+    run.debounce(this, () => {
+      this.column.addFilters(
+        'search', {
+          alias: 'search',
+          term: this._searchQuery,
+        }
+      );
+    }, 1000);
+  }),
 
   actions: {
     orderingOptionChanged(value) {
