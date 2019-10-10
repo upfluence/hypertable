1f5f84d3cf547ed348913e580e6005849d1c3639
Mohamed Hegazy <m.hegazy94@hotmail.com>
2019-10-10 18:12:53 +0200

    Refactoring mixin

--- addon/mixins/editable.js
+++ addon/mixins/editable.js
@@ -3,15 +3,14 @@ import { run } from '@ember/runloop';
 
 export default Mixin.create({
   isEditing: false,
-  
+
   actions: {
     toggleEditing() {
-      let self = this;
       this.toggleProperty("isEditing");
       
       if(this.get('isEditing')) {
-        run.scheduleOnce('afterRender', this, function() {
-          self.$('.editing-input__field').focus();
+        run.scheduleOnce('afterRender', this, () => {
+          this.$('.editing-input__field').focus();
         });
       } else {
         this.manager.updateColumnValue(this.column.property, this.item, this.value);
