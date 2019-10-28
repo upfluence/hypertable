f2bb1473349ba8bea914c56198b1ef351b1d1b4a
Philippe Ndiaye <phndiaye@gmail.com>
2019-10-28 11:26:32 +0100

    make the column upsert feature config based

--- addon/mixins/editable.js
+++ addon/mixins/editable.js
@@ -6,8 +6,8 @@ export default Mixin.create({
 
   actions: {
     toggleEditing(value) {
-      this.toggleProperty("isEditing");
-      
+      this.toggleProperty('isEditing');
+
       if(this.get('isEditing')) {
         run.scheduleOnce('afterRender', this, () => {
           this.$('.editing-input__field').focus();
@@ -16,6 +16,7 @@ export default Mixin.create({
         this.manager.updateColumnValue(this.column.property, this.item, value);
       }
     },
+
     updateCurrency(currency) {
       this.manager.updateColumnValue(this.column.currency_key, this.item, currency);
     }
