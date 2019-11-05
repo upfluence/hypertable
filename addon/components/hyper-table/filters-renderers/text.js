a3b686c8b1401debb9e21809b83e20fed4da3d49
Philippe Ndiaye <phndiaye@gmail.com>
2019-11-05 17:36:01 +0100

    update the way the text column term filter is handled

--- addon/components/hyper-table/filters-renderers/text.js
+++ addon/components/hyper-table/filters-renderers/text.js
@@ -6,9 +6,10 @@ export default Component.extend({
   classNames: ['available-filters'],
 
   _searchQuery: computed('column', 'column.filters', function() {
-    let searchTerm = this.column.filters.find((x)=> x.value.term);
+    let searchTerm = this.column.filters.findBy('key', 'value');
 
-    return searchTerm ? searchTerm.value.term : null;
+
+    return searchTerm ? searchTerm.value : null;
   }),
 
   orderingOptions: computed('column.orderKey', function() {
@@ -19,12 +20,7 @@ export default Component.extend({
   }),
 
   _addSearchFilter() {
-    this.column.addFilters(
-      'search', {
-        alias: 'search',
-        term: this._searchQuery,
-      }
-    );
+    this.column.set('filters', [{ key: 'value', value: this._searchQuery }]);
   },
 
   _searchQueryObserver: observer('_searchQuery', function() {
