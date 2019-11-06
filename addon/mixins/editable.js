26102780c248f4f39c7700806d1747e047cab203
Mohamed Hegazy <m.hegazy94@hotmail.com>
2019-11-06 18:06:43 +0100

    Fix bug when editing multiple inputs on the same line

--- addon/mixins/editable.js
+++ addon/mixins/editable.js
@@ -1,15 +1,23 @@
 import Mixin from '@ember/object/mixin';
+import { computed } from '@ember/object';
 import { run } from '@ember/runloop';
 
 export default Mixin.create({
+
+  editStatus: computed('item.editStatus', 'column.key', function() {
+    if(this.column.key === this.get('item.editStatus.key')) {
+      return this.item.editStatus;
+    }
+  }),
+
   actions: {
     toggleEditing(value) {
-      if(!this.get('item.editStatus.status')){
+      if(!this.get('editStatus.status')){
         this.item.set('editStatus', {key: this.column.key, status: 'editing'})
         run.scheduleOnce('afterRender', this, () => {
           this.$('.editing-input__field').focus();
         });
-      } else if(this.get('item.editStatus.status') !== 'success') {
+      } else if(this.get('editStatus.status') !== 'success') {
         run.once(this, () => {
           this.item.set('editStatus', {key: this.column.key, status: 'saving'});
           this.manager.updateColumnValue(this.column.key, this.item, value);
