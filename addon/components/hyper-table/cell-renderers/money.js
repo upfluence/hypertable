341b7b7f4e1541fb6b78ef1589fdd00ca4e99a55
Philippe Ndiaye <phndiaye@gmail.com>
2019-11-20 10:49:29 +0100

    money is received as cents

--- addon/components/hyper-table/cell-renderers/money.js
+++ addon/components/hyper-table/cell-renderers/money.js
@@ -6,7 +6,7 @@ import CellRendererMixin from '@upfluence/hypertable/mixins/cell-renderer';
 
 export default Component.extend(EditableMixin, CellRendererMixin, {
   amount: computed('value', function() {
-    return (this.value) ? (parseInt(this.value.cents) || 0) / 10000 : null;
+    return (this.value) ? (parseInt(this.value.cents) || 0) / 100 : null;
   }),
 
   currency: computed('value', function() {
