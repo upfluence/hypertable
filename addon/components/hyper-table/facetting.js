53da9b28cccccc90be9f94f3ae9ad2fda34164b7
Philippe Ndiaye <phndiaye@gmail.com>
2019-12-18 11:21:34 +0100

    add facetting loading state

--- addon/components/hyper-table/facetting.js
+++ addon/components/hyper-table/facetting.js
@@ -1,16 +1,20 @@
 import Component from '@ember/component';
 import { isEmpty } from '@ember/utils';
+import { run } from '@ember/runloop';
 
 export default Component.extend({
   classNames: ['hypertable__facetting'],
+  classNameBindings: ['loading:hypertable__facetting--loading'],
 
   column: null,
   filteringKey: null,
   facets: [],
 
+  loadingFacetsRange: new Array(8),
+
   actions: {
     toggleAppliedFacet(facet) {
-      Ember.run.later(() => {
+      run.later(() => {
         this.column.applyFacets(
           this.filteringKey, this.facets.filter((f) => {
             return f.applied && !isEmpty(f.identifier);
