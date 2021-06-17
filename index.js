630d52e036fdd4bb5cd307ada88ecaab9d42b825
Philippe Ndiaye <phndiaye@gmail.com>
2021-06-17 09:57:33 +0200

    register hypertable version in Ember.libraries (useful for debugging)

--- index.js
+++ index.js
@@ -1,12 +1,37 @@
 'use strict';
 
-const { name, version } = require('./package');
+const { name, version } = require('./package');
+const MergeTrees = require('broccoli-merge-trees');
+const writeFile = require('broccoli-file-creator');
 
 module.exports = {
   name,
   version,
 
-  isDevelopingAddon: function() {
+  isDevelopingAddon: function () {
     return true;
-  }
+  },
+
+  included: function () {
+    this._super.included.apply(this, arguments);
+
+    this.import('vendor/@upfluence/hypertable/register-version.js');
+  },
+
+  treeForVendor(vendorTree) {
+    let trees = [];
+
+    if (vendorTree) {
+      trees.push(vendorTree);
+    }
+
+    trees.push(
+      writeFile(
+        '@upfluence/hypertable/register-version.js',
+        `Ember.libraries.register('${name}', '${version}');`
+      )
+    );
+
+    return new MergeTrees(trees);
+  },
 };
