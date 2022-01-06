70c22bdce62481270a86b0d236d89ee5c3f0f386
Philippe Ndiaye <phndiaye@gmail.com>
2022-01-06 18:04:37 +0100

    cleanup

--- addon/components/hyper-table-v2.ts
+++ addon/components/hyper-table-v2.ts
@@ -5,11 +5,11 @@ import {tracked} from '@glimmer/tracking';
 interface HyperTableV2Args {}
 
 export default class HyperTableV2 extends Component<HyperTableV2Args> {
-  @tracked columns = []; 
-
   constructor(owner, args) {
     super(owner, args);
 
-    args.handler.fetchColumns()
+    args.handler.fetchColumns().then(() => {
+      args.handler.fetchRows();
+    });
   }
 }
