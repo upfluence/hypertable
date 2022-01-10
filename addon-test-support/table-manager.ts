27617a276a07e3daf8f3c3beb01bd767354918d9
Philippe Ndiaye <phndiaye@gmail.com>
2022-01-10 11:57:22 +0100

    add table handler class

---
+++ addon-test-support/table-manager.ts
@@ -0,0 +1,43 @@
+import {
+  Column,
+  ColumnDefinitionResponse,
+  FieldSize,
+  TableColumnsResponse,
+  TableManager as ITableManager,
+  TableColumnUpsertRequest,
+  TableColumnUpsertResponse
+} from '@upfluence/hypertable/core/interfaces';
+
+const buildColumn = (key: string): Column => {
+  const def = {
+    key,
+    type: 'text',
+    name: `${key}_name`,
+    clustering_key: '',
+    category: '',
+    size: FieldSize.Medium,
+    orderable: false,
+    filterable: false,
+    facetable: false
+  };
+
+  return {
+    definition: def,
+    filters: []
+  };
+};
+
+export default class TableManager implements ITableManager {
+  fetchColumnDefinitions(): Promise<ColumnDefinitionResponse> {
+    return Promise.resolve({ column_definitions: [] });
+  }
+
+  fetchColumns(): Promise<TableColumnsResponse> {
+    return Promise.resolve({ columns: [buildColumn('foo'), buildColumn('bar')] });
+  }
+
+  // @ts-ignore
+  upsertColumns(request: TableColumnUpsertRequest): Promise<TableColumnUpsertResponse> {
+    return Promise.resolve({ columns: [buildColumn('foo'), buildColumn('bar')] });
+  }
+}
