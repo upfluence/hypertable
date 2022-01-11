c14a0ee8886dc6bb5c45625b3ac58e182dd36ee5
Philippe Ndiaye <phndiaye@gmail.com>
2022-01-11 15:03:18 +0100

    add a basic rendering resolver

---
+++ addon/components/hyper-table-v2/header-renderers/base.ts
@@ -0,0 +1,11 @@
+import Component from '@glimmer/component';
+import TableHandler from '@upfluence/hypertable/core/handler';
+import { Column } from '@upfluence/hypertable/core/interfaces';
+
+interface HyperTableV2HeaderRenderersBaseArgs {
+  handler: TableHandler;
+  column: Column;
+  extra?: { [key: string]: any };
+}
+
+export default class HyperTableV2HeaderRenderersBase extends Component<HyperTableV2HeaderRenderersBaseArgs> {}
