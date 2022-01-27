f9311535b5ad578517eebecf44e44cfbb4422e9a
MBrunelle <maximilien.brunelle@gmail.com>
2022-01-27 08:41:06 +0100

    Added: apply Range filter on input change

--- addon/components/hyper-table-v2/filtering-renderers/numeric.ts
+++ addon/components/hyper-table-v2/filtering-renderers/numeric.ts
@@ -4,6 +4,7 @@ import { action } from '@ember/object';
 
 import TableHandler from '@upfluence/hypertable/core/handler';
 import { Column, OrderDirection } from '@upfluence/hypertable/core/interfaces';
+import { run } from '@ember/runloop';
 
 interface HyperTableV2FilteringRenderersNumericArgs {
   handler: TableHandler;
@@ -38,14 +39,28 @@ export default class HyperTableV2FilteringRenderersNumeric extends Component<Hyp
     return this.lowerBoundFilter || this.upperBoundFilter ? 'with' : null;
   }
 
+  private _addRangeFilter(): void {
+    this.args.handler.applyFilters(this.args.column, [
+      ...(this.lowerBoundFilter ? [{ key: 'lower_bound', value: this.lowerBoundFilter }] : []),
+      ...(this.upperBoundFilter ? [{ key: 'upper_bound', value: this.upperBoundFilter }] : [])
+    ]);
+  }
+
+  @action
+  addRangeFilter(): void {
+    run.debounce(this, this._addRangeFilter, 1000);
+  }
+
   @action
   orderingDirectionChanged(direction: OrderDirection): void {
+    console.log('orderingDirectionChanged', direction);
     // Triggers twice on click, why ? Probably the same in the other filter renderers, needs checking
     this.args.handler.applyOrder(this.args.column, direction);
   }
 
   @action
   existenceFilterChanged(value: string): void {
+    console.log('existenceFilterChanged', value);
     // Triggers twice on click, why ? Probably the same in the other filter renderers, needs checking
     this.args.handler.applyFilters(this.args.column, [{ key: 'existence', value }]);
   }
