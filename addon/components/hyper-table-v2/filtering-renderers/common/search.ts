ddafdf74156ce4d6b24386a62431dd93ea9116cb
MBrunelle <maximilien.brunelle@gmail.com>
2022-02-01 15:43:04 +0100

    Added: Search Common component for filter renderers

---
+++ addon/components/hyper-table-v2/filtering-renderers/common/search.ts
@@ -0,0 +1,57 @@
+import { action } from '@ember/object';
+import { debounce } from '@ember/runloop';
+import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
+
+import TableHandler from '@upfluence/hypertable/core/handler';
+import { Column } from '@upfluence/hypertable/core/interfaces';
+
+interface HyperTableV2FilteringRenderersSearchArgs {
+  handler: TableHandler;
+  column: Column;
+  registerResetCallback: any;
+}
+
+const SEARCH_DEBOUNCE_TIME: number = 300;
+
+export default class HyperTableV2FilteringRenderersSearch extends Component<HyperTableV2FilteringRenderersSearchArgs> {
+  @tracked searchQuery: string = '';
+
+  constructor(owner: unknown, args: HyperTableV2FilteringRenderersSearchArgs) {
+    super(owner, args);
+
+    if (typeof this.args.registerResetCallback !== 'function') {
+      throw new Error(
+        '[HyperTableV2::FilteringRenderers::Common::Search] You need to link a method to reset your search input'
+      );
+    }
+    const searchTerm = this.args.column.filters.find((filter) => filter.key === 'value');
+    this.searchQuery = searchTerm?.value || '';
+    this.args.registerResetCallback(this._resetSearchQuery.bind(this));
+  }
+
+  @action
+  onInputChanged(): void {
+    debounce(this, this._applyFilters, SEARCH_DEBOUNCE_TIME);
+  }
+
+  @action
+  onClearSearch(event: MouseEvent): void {
+    event.stopPropagation();
+    this.searchQuery = '';
+    this._applyFilters();
+  }
+
+  private _applyFilters(): void {
+    this.args.handler.applyFilters(this.args.column, [
+      {
+        key: 'value',
+        value: this.searchQuery
+      }
+    ]);
+  }
+
+  private _resetSearchQuery(): void {
+    this.searchQuery = '';
+  }
+}
---
+++ tests/integration/components/hyper-table-v2/filtering-renderers/common/search-test.ts
@@ -0,0 +1,116 @@
+import { module, test } from 'qunit';
+import { setupRenderingTest } from 'ember-qunit';
+import { click, fillIn, render, triggerKeyEvent } from '@ember/test-helpers';
+import hbs from 'htmlbars-inline-precompile';
+import sinon from 'sinon';
+
+import TableHandler from '@upfluence/hypertable/core/handler';
+import { TableManager, RowsFetcher } from '@upfluence/hypertable/test-support';
+import { buildColumn } from '@upfluence/hypertable/test-support/table-manager';
+import { FieldSize } from '@upfluence/hypertable/core/interfaces';
+import settled from '@ember/test-helpers/settled';
+
+module('Integration | Component | hyper-table-v2/filtering-renderers/common/search', function (hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(async function () {
+    this.tableManager = new TableManager();
+    this.rowsFetcher = new RowsFetcher();
+    this.handler = new TableHandler(this, this.tableManager, this.rowsFetcher);
+
+    sinon.stub(this.tableManager, 'fetchColumns').callsFake(() => {
+      return Promise.resolve({
+        columns: [buildColumn('Name', { type: 'text', size: FieldSize.Large, filterable: true, orderable: true })]
+      });
+    });
+
+    await this.handler.fetchColumns();
+
+    this.column = this.handler.columns[0];
+    this.registerCallback = () => {};
+  });
+
+  test('it has the right data-control-name', async function (assert: Assert) {
+    await render(
+      hbs`<HyperTableV2::FilteringRenderers::Common::Search @handler={{this.handler}} @column={{this.column}} @registerResetCallback={{this.registerCallback}} />`
+    );
+    assert.dom('.oss-input-container').exists();
+  });
+
+  test('When text is inputed, the applyFilters is called', async function (assert: Assert) {
+    const handlerSpy = sinon.spy(this.handler);
+    await render(
+      hbs`<HyperTableV2::FilteringRenderers::Common::Search @handler={{this.handler}} @column={{this.column}} @registerResetCallback={{this.registerCallback}} />`
+    );
+
+    await fillIn('.oss-input-container input', 'test');
+    await triggerKeyEvent(
+      '.oss-input-container input',
+      'keyup',
+      'Enter',
+      //@ts-ignore
+      { code: 'Enter' }
+    );
+
+    assert.ok(
+      //@ts-ignore
+      handlerSpy.applyFilters.calledWith(this.column, [
+        {
+          key: 'value',
+          value: 'test'
+        }
+      ])
+    );
+  });
+
+  test('The remove icon is displayed when text is entered', async function (assert: Assert) {
+    await render(
+      hbs`<HyperTableV2::FilteringRenderers::Common::Search @handler={{this.handler}} @column={{this.column}} @registerResetCallback={{this.registerCallback}} />`
+    );
+    assert.dom('.oss-input-container').exists();
+    await fillIn('.oss-input-container input', 'test');
+    assert.dom('.oss-input-container .suffix .fa-remove').exists();
+  });
+
+  test('The remove icon is hidden when the input is empty', async function (assert: Assert) {
+    await render(
+      hbs`<HyperTableV2::FilteringRenderers::Common::Search @handler={{this.handler}} @column={{this.column}} @registerResetCallback={{this.registerCallback}} />`
+    );
+    assert.dom('.oss-input-container').exists();
+    assert.dom('.oss-input-container .suffix .fa-remove').doesNotExist();
+  });
+
+  test('When the remove icon is clicked, the text input is cleared, #handler.applyFilters is triggered', async function (assert: Assert) {
+    const handlerSpy = sinon.spy(this.handler);
+    await render(
+      hbs`<HyperTableV2::FilteringRenderers::Common::Search @handler={{this.handler}} @column={{this.column}} @registerResetCallback={{this.registerCallback}} />`
+    );
+    assert.dom('.oss-input-container').exists();
+    await fillIn('.oss-input-container input', 'test');
+    await click('.oss-input-container .suffix .fa-remove');
+    assert.ok(
+      //@ts-ignore
+      handlerSpy.applyFilters.calledWith(this.column, [
+        {
+          key: 'value',
+          value: ''
+        }
+      ])
+    );
+  });
+
+  test('When the parent triggers the reset event, the text input is cleared', async function (assert: Assert) {
+    this.resetTrigger = (childResetMethod: () => {}) => {
+      this._childResetCallback = childResetMethod;
+    };
+    await render(
+      hbs`<HyperTableV2::FilteringRenderers::Common::Search @handler={{this.handler}} @column={{this.column}} @registerResetCallback={{this.resetTrigger}} />`
+    );
+    assert.dom('.oss-input-container').exists();
+    await fillIn('.oss-input-container input', 'test');
+    assert.dom('.oss-input-container input').hasValue('test');
+    this._childResetCallback();
+    await settled();
+    assert.dom('.oss-input-container input').hasValue('');
+  });
+});
