2295dc7d8082e294fabb69202cb937abbcc752ef
MBrunelle <maximilien.brunelle@gmail.com>
2022-02-07 16:14:57 +0100

    Added: Existence & Ordering now properly redraw upon clicking

--- addon/components/hyper-table-v2/filtering-renderers/common/ordering.ts
+++ addon/components/hyper-table-v2/filtering-renderers/common/ordering.ts
@@ -1,5 +1,6 @@
 import { action } from '@ember/object';
 import Component from '@glimmer/component';
+import { tracked } from '@glimmer/tracking';
 
 import TableHandler from '@upfluence/hypertable/core/handler';
 import { Column, OrderDirection } from '@upfluence/hypertable/core/interfaces';
@@ -16,16 +17,30 @@ const defaultOrderingDirections: { [key: string]: OrderDirection } = {
 };
 
 export default class HyperTableV2FilteringRenderersOrdering extends Component<HyperTableV2FilteringRenderersOrderingArgs> {
+  @tracked _selectedDirection: OrderDirection | undefined;
+
+  constructor(owner: unknown, args: HyperTableV2FilteringRenderersOrderingArgs) {
+    super(owner, args);
+
+    args.handler.on('reset-columns', (columns) => {
+      if (columns.includes(args.column)) {
+        this._selectedDirection = undefined;
+      }
+    });
+  }
+
   get orderingOptions() {
     return this.args?.orderingOptions || defaultOrderingDirections;
   }
 
   get currentOrderingDirection(): OrderDirection | undefined {
-    return this.args.column.order?.direction;
+    return this._selectedDirection || this.args.column.order?.direction;
   }
 
   @action
   orderingDirectionChanged(direction: OrderDirection): void {
-    this.args.handler.applyOrder(this.args.column, direction);
+    this.args.handler.applyOrder(this.args.column, direction).then(() => {
+      this._selectedDirection = direction;
+    });
   }
 }
