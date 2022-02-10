84a7ad6c7c03d13ef4294e10efbc399b3db5561c
MBrunelle <maximilien.brunelle@gmail.com>
2022-02-10 09:51:28 +0100

    Added: Common::ColumnActions component (reset & remove buttons)

---
+++ addon/components/hyper-table-v2/filtering-renderers/common/column-actions.ts
@@ -0,0 +1,27 @@
+import Component from '@glimmer/component';
+import { action } from '@ember/object';
+
+import TableHandler from '@upfluence/hypertable/core/handler';
+import { Column } from '@upfluence/hypertable/core/interfaces';
+
+interface HyperTableV2FilteringRenderersCommonColumnActionsArgs {
+  handler: TableHandler;
+  column: Column;
+}
+
+export default class HyperTableV2FilteringRenderersCommonColumnActions extends Component<HyperTableV2FilteringRenderersCommonColumnActionsArgs> {
+  get shouldDisplay(): boolean {
+    return this.args.handler.columnPosition(this.args.column) !== 0;
+  }
+
+  @action
+  reset(): void {
+    this.args.handler.resetColumns([this.args.column]);
+  }
+
+  @action
+  removeColumn(): void {
+    this.args.handler.destroyTetherInstance();
+    this.args.handler.removeColumn(this.args.column.definition);
+  }
+}
