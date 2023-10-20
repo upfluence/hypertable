970c61fa3770f257ed23ce1849a7d361768ccf2e
Philippe Ndiaye <phndiaye@gmail.com>
2023-10-20 11:02:31 +0200

    dynamically watch for changes on the text columns

--- addon/components/hyper-table-v2/cell-renderers/text.ts
+++ addon/components/hyper-table-v2/cell-renderers/text.ts
@@ -1,4 +1,5 @@
 import Component from '@glimmer/component';
+import { computed, defineProperty } from '@ember/object';
 
 import TableHandler from '@upfluence/hypertable/core/handler';
 import { Column, Row } from '@upfluence/hypertable/core/interfaces';
@@ -11,7 +12,15 @@ interface HyperTableV2CellRenderersTextArgs {
 }
 
 export default class HyperTableV2CellRenderersText extends Component<HyperTableV2CellRenderersTextArgs> {
-  get value(): string {
-    return this.args.row[this.args.column.definition.key];
+  constructor(owner: unknown, args: HyperTableV2CellRenderersTextArgs) {
+    super(owner, args);
+
+    defineProperty(
+      this,
+      'value',
+      computed(`args.row.${args.column.definition.key}`, function() {
+        return this.args.row[this.args.column.definition.key];
+      })
+    );
   }
 }
