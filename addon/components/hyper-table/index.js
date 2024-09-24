f94e43f5c4964603dc5512adaa21ef9af39b1577
nathalie <nathalie.arnoux@upfluence.com>
2024-09-24 16:11:11 +0200

    Fixed: PR feedback on function name

--- addon/components/hyper-table/index.js
+++ addon/components/hyper-table/index.js
@@ -212,7 +212,7 @@ export default Component.extend({
       this._doSearch();
     },
 
-    centerElement(element) {
+    scrollToEmptyState(element) {
       element?.scrollIntoView({ inline: 'center' });
     },
 
