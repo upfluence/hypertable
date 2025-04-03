76cc558e18837e1a42acc0b23c55b1a6b37ac22e
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2025-04-03 15:40:24 +0200

    add setup-table test-support util

--- addon-test-support/index.ts
+++ addon-test-support/index.ts
@@ -1,4 +1,5 @@
 import TableManager from './table-manager';
 import RowsFetcher, { AllRowsFetcher } from './rows-fetcher';
+import { setupTable } from './setup-table';
 
-export { TableManager, RowsFetcher, AllRowsFetcher };
+export { TableManager, RowsFetcher, AllRowsFetcher, setupTable };
