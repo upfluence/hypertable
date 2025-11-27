a599f9db843d8119004432015209305d7f07ff9a
Philippe Ndiaye <philippe.ndiaye@upfluence.com>
2025-11-27 09:45:31 +0100

    ci: Setup compatibility checks workflow

--- config/ember-try.js
+++ config/ember-try.js
@@ -1,94 +1,22 @@
 'use strict';
 
-const getChannelURL = require('ember-source-channel-url');
-const { embroiderSafe, embroiderOptimized } = require('@embroider/test-setup');
-
 module.exports = async function () {
   return {
+    packageManager: 'pnpm',
+    command: 'ember test --silent -r dot',
     scenarios: [
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.24.3'
-          }
-        }
+        name: 'ember-lts-3.28'
       },
       {
-        name: 'ember-lts-3.28',
+        name: 'ember-4.12',
         npm: {
           devDependencies: {
-            'ember-source': '~3.28.0'
+            'ember-source': '~4.12.3',
+            'ember-cli': '~4.12.3'
           }
         }
-      },
-      {
-        name: 'ember-release',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('release')
-          }
-        }
-      },
-      {
-        name: 'ember-beta',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('beta')
-          }
-        }
-      },
-      {
-        name: 'ember-canary',
-        npm: {
-          devDependencies: {
-            'ember-source': await getChannelURL('canary')
-          }
-        }
-      },
-      // The default `.travis.yml` runs this scenario via `npm test`,
-      // not via `ember try`. It's still included here so that running
-      // `ember try:each` manually or from a customized CI config will run it
-      // along with all the other scenarios.
-      {
-        name: 'ember-default',
-        npm: {
-          devDependencies: {}
-        }
-      },
-      {
-        name: 'ember-default-with-jquery',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'jquery-integration': true
-          })
-        },
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0'
-          }
-        }
-      },
-      {
-        name: 'ember-classic',
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': true,
-            'default-async-observers': false,
-            'template-only-glimmer-components': false
-          })
-        },
-        npm: {
-          devDependencies: {
-            'ember-source': '~3.28.0'
-          },
-          ember: {
-            edition: 'classic'
-          }
-        }
-      },
-      embroiderSafe(),
-      embroiderOptimized()
+      }
     ]
   };
 };
