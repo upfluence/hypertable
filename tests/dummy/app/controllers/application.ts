f423f3073663890a30760eb074a385d2c1bf6621
Maximilien B. <maximilien.brunelle@gmail.com>
2026-09-21 16:16:50 +0200

    DummyApp - add buttons to reset rows and to disable loading animations - introduce fake data loading time

--- tests/dummy/app/controllers/application.ts
+++ tests/dummy/app/controllers/application.ts
@@ -11,7 +11,8 @@ import {
   TableColumnUpsertResponse,
   FieldSize,
   TableManager,
-  TableColumnsResponse
+  TableColumnsResponse,
+  RowsFetcherResponse
 } from '@upfluence/hypertable/core/interfaces';
 
 const columnDefinitions = [
@@ -97,9 +98,10 @@ class Manager implements TableManager {
 }
 
 class RowsFetcher {
-  fetch(_page: number, _perPage: number) {
-    return Promise.resolve({
-      rows: [
+  fetch(_page: number, _perPage: number): Promise<RowsFetcherResponse> {
+    return new Promise<RowsFetcherResponse>((resolve) => {
+      window.setTimeout(() => resolve({
+        rows: [
         {
           influencerId: Math.random(),
           recordId: 12,
@@ -205,8 +207,9 @@ class RowsFetcher {
           foo: 'second',
           bar: 'second bar'
         }
-      ],
-      meta: { total: 12 }
+        ],
+        meta: { total: 12 }
+      }), 500);
     });
   }
 }
@@ -214,6 +217,7 @@ class RowsFetcher {
 export default class Application extends Controller {
   @tracked searchQuery: string = '';
   @tracked selectedValue: number = 0;
+  @tracked animationsEnabled: boolean = true;
   private prependedRowsCount: number = 0;
 
   tableManager = new Manager();
@@ -230,16 +234,16 @@ export default class Application extends Controller {
 
   get tableOptions() {
     return {
-      initialLoadAnimation: {
-        delayMs: 50,
-        staggerMs: 150,
-        maxAnimationDurationMs: 5000,
-        extraColumnEffect: {
-          class: 'smart-rotating-gradient',
-          delayMs: 250,
-          columns: ['foo']
-        }
-      }
+      initialLoadAnimation: this.animationsEnabled
+        ? {
+            staggerMs: 150,
+            replayOn: ['prepend-rows', 'reset-rows'],
+            extraColumnEffect: {
+              class: 'smart-rotating-gradient',
+              columns: ['foo']
+            }
+          }
+        : false
     };
   }
 
@@ -259,6 +263,11 @@ export default class Application extends Controller {
   }
 
   @action
+  toggleAnimations(value: boolean): void {
+    this.animationsEnabled = value;
+  }
+
+  @action
   prependRows(): void {
     const firstRecordId = 100 + this.prependedRowsCount;
     this.prependedRowsCount += 2;
@@ -267,6 +276,7 @@ export default class Application extends Controller {
       {
         influencerId: firstRecordId,
         recordId: firstRecordId,
+        record_id: firstRecordId,
         holderId: 57,
         holderType: 'list',
         foo: `Prepended ${firstRecordId}`,
@@ -275,12 +285,18 @@ export default class Application extends Controller {
       {
         influencerId: firstRecordId + 1,
         recordId: firstRecordId + 1,
+        record_id: firstRecordId + 1,
         holderId: 57,
         holderType: 'list',
         foo: `Prepended ${firstRecordId + 1}`,
         bar: 'New row'
       }
     ]);
+  }
+
+  @action
+  resetRows(): Promise<void> {
+    return this.handler.resetRows();
   }
 
   @action
